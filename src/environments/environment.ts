79840c788d23018b467cc46bca6862abe36273c1
Kevin Qu <kevinqu88@gmail.com>
2020-06-23 09:22:33 -0400

    firebase deploy added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCwCB_LB7HYrCPw6FcEEuK7flE_kR3T9xk",
+    authDomain: "kq-1-1a499.firebaseapp.com",
+    databaseURL: "https://kq-1-1a499.firebaseio.com",
+    projectId: "kq-1-1a499",
+    storageBucket: "kq-1-1a499.appspot.com",
+    messagingSenderId: "370493268596",
+    appId: "1:370493268596:web:55e44811e2f90c85882721",
+  }
 };
 
 /*
